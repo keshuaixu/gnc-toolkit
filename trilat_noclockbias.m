90eb76fd5cf1cb0eac623f9ffc8ca3fedfe2c880
Cosine <urillx@gmail.com>
2017-08-31 03:49:47 -0400

    formatting

--- trilat_noclockbias.m
+++ trilat_noclockbias.m
@@ -1,11 +1,11 @@
-function [ receiver_position ] = trilat_noclockbias( satellite_positions, measured_ranges, initial_guess, e1, e2, max_iter )
+function [receiver_position] = trilat_noclockbias(satellite_positions, measured_ranges, initial_guess, e1, e2, max_iter)
 %TRILAT_NOCLOCKBIAS Summary of this function goes here
 %   Detailed explanation goes here
 
 big_fat_one = @(p_r_guess, p_sat) (p_r_guess - p_sat) / norm(p_r_guess - p_sat);
 receiver_position = initial_guess;
 
-for iter=1:max_iter
+for iter = 1:max_iter
     A = cell2mat(cellfun(@(p_sat) big_fat_one(receiver_position, p_sat)', num2cell(satellite_positions, 1), 'UniformOutput', false)');
     b = measured_ranges - cellfun(@norm, num2cell(bsxfun(@minus, receiver_position, satellite_positions), 1))';
     d_p = A \ b;
